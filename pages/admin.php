debc5077f52ec00f0a306da02f8ec48e4a1bee3b
Wipawee <wipawee.jai@ku.th>
2024-02-14 22:22:56 +0700

    เปลี่ยนชื่อหน้า

--- pages/admin.php
+++ pages/admin.php
@@ -37,9 +37,9 @@
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 ps-2 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="text-white opacity-5" href="javascript:;">หน้า</a></li>
-            <li class="breadcrumb-item text-sm text-white" aria-current="page">Admin</li>
+            <li class="breadcrumb-item text-sm text-white" aria-current="page">จัดการกิจกรรม</li>
           </ol>
-          <h6 class="text-white font-weight-bolder ms-2">Admin</h6>
+          <h6 class="text-white font-weight-bolder ms-2">จัดการกิจกรรม</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
--- pages/navbar.php
+++ pages/navbar.php
@@ -195,7 +195,7 @@
               <a class="nav-link <?php echo ($current_page == $admin_url) ? 'active' : ''; ?>" href="<?php echo $admin_url; ?>">
               <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                 <svg width="12px" height="12px" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                  <title>settings</title>
+                  <title>จัดการกิจกรรม</title>
                   <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                     <g transform="translate(-2020.000000, -442.000000)" fill="#FFFFFF" fill-rule="nonzero">
                       <g transform="translate(1716.000000, 291.000000)">
@@ -209,7 +209,7 @@
                   </g>
                 </svg>
               </div>
-              <span class="nav-link-text ms-1">แอดมิน</span>
+              <span class="nav-link-text ms-1">จัดการกิจกรรม</span>
               </a>
             </li>
 
